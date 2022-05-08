d4c3461736364891b1a5419a313f0b2bbb186392
Joel Weber <joel@joelweber.nl>
2022-05-08 13:54:56 +0200

    Code style changes

--- sentry.server.config.js
+++ sentry.server.config.js
@@ -7,7 +7,9 @@ import * as Sentry from '@sentry/nextjs';
 const SENTRY_DSN = process.env.SENTRY_DSN || process.env.NEXT_PUBLIC_SENTRY_DSN;
 
 Sentry.init({
-  dsn: SENTRY_DSN || 'https://b8c0b54da3824d709277b47984f6e495@o501435.ingest.sentry.io/6384833',
+  dsn:
+    SENTRY_DSN ||
+    'https://b8c0b54da3824d709277b47984f6e495@o501435.ingest.sentry.io/6384833',
   // Adjust this value in production, or use tracesSampler for greater control
   tracesSampleRate: 1.0,
   // ...
