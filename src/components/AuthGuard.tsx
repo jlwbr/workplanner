3f9e2023374c174e97e92aa688dc98f9a5242e60
Joel Weber <joel@joelweber.nl>
2022-05-13 14:44:45 +0200

    Add spinners to make loading more clear

--- src/components/AuthGuard.tsx
+++ src/components/AuthGuard.tsx
@@ -16,7 +16,30 @@ export function AuthGuard({ children }: { children: ReactNode }) {
 
   /* show loading indicator while the auth provider is still initializing */
   if (status === 'loading') {
-    return <h1>Application Loading</h1>;
+    return (
+      <div className="flex flex-col items-center justify-center h-screen w-screen bg-slate-200">
+        <svg
+          className="animate-spin -ml-1 mr-3 h-20 w-20 text-white"
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+        >
+          <circle
+            className="opacity-25"
+            cx="12"
+            cy="12"
+            r="10"
+            stroke="currentColor"
+            strokeWidth="4"
+          ></circle>
+          <path
+            className="opacity-75"
+            fill="currentColor"
+            d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+          ></path>
+        </svg>
+      </div>
+    );
   }
 
   // if auth initialized with a valid user show protected page
