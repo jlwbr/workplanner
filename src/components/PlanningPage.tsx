d572affea463f2357800fe7f038ebe71d423636f
Joel Weber <joel@joelweber.nl>
2022-05-19 20:29:09 +0200

    feat(PlanningPage): Add loading spinner

--- src/components/PlanningPage.tsx
+++ src/components/PlanningPage.tsx
@@ -17,6 +17,31 @@ type PrintComponentType = {
   date: Date;
 };
 
+const Loading = () => (
+  <div className="flex flex-col items-center justify-center h-20 w-20">
+    <svg
+      className="animate-spin h-20 w-20 text-slate-400"
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+    >
+      <circle
+        className="opacity-25"
+        cx="12"
+        cy="12"
+        r="10"
+        stroke="currentColor"
+        strokeWidth="4"
+      ></circle>
+      <path
+        className="opacity-75"
+        fill="currentColor"
+        d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+      ></path>
+    </svg>
+  </div>
+);
+
 const PrintComponent = forwardRef<HTMLDivElement, PrintComponentType>(
   ({ date }, ref) => {
     const planing = trpc.useQuery([
@@ -59,16 +84,16 @@ const PrintComponent = forwardRef<HTMLDivElement, PrintComponentType>(
       { date, TimeOffDay: 'EVENING' },
     ]);
 
-    if (!planing.isSuccess || !planing.data) return null;
-    if (!MorningBreak.isSuccess || !MorningBreak.data) return null;
-    if (!AfternoonBreak.isSuccess || !AfternoonBreak.data) return null;
-    if (!EveningBreak.isSuccess || !EveningBreak.data) return null;
+    if (!planing.isSuccess || !planing.data) return <Loading />;
+    if (!MorningBreak.isSuccess || !MorningBreak.data) return <Loading />;
+    if (!AfternoonBreak.isSuccess || !AfternoonBreak.data) return <Loading />;
+    if (!EveningBreak.isSuccess || !EveningBreak.data) return <Loading />;
     if (!MorningCommunication.isSuccess || !MorningCommunication.data)
-      return null;
+      return <Loading />;
     if (!AfternoonCommunication.isSuccess || !AfternoonCommunication.data)
-      return null;
+      return <Loading />;
     if (!EveningCommunication.isSuccess || !EveningCommunication.data)
-      return null;
+      return <Loading />;
 
     const MorningBreakData = groupByKey(MorningBreak.data, 'userId');
     const AfternoonBreakData = groupByKey(AfternoonBreak.data, 'userId');
