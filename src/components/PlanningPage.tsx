c215cad3225337d6bf3ffeb8aefb2f615ce2d4ad
Joel Weber <joel@joelweber.nl>
2022-05-20 13:43:26 +0200

    feat(PlanningPage): Add extra margin to header

--- src/components/PlanningPage.tsx
+++ src/components/PlanningPage.tsx
@@ -115,7 +115,7 @@ const PrintComponent = forwardRef<HTMLDivElement, PrintComponentType>(
     return (
       <div ref={ref}>
         <style>{'@page { margin: 2rem !important; }'}</style>
-        <div className="flex gap-2 pb-5">
+        <div className="flex gap-5 pb-5">
           <div className="flex-1">
             <h1 className="text-2xl font-bold">Dagverdeling</h1>
             <h2 className="text-lg">
